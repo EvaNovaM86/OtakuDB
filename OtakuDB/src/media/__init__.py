6b12e7c806164df6242005c41ffe3a261d2b2f43
Evelyn Young <evanovam86@gmail.com>
2016-04-21 01:41:44 -0600

    Update __init__.py

--- OtakuDB/src/media/__init__.py
+++ OtakuDB/src/media/__init__.py
@@ -2,7 +2,6 @@ import webbrowser
 
 class Movie():
     __doc__ = 'This class provides a way to store movie information'
-    VALID_RATINGS = ['G','PG','PG-13','R']
 
     def __init__(self, movie_title,movie_storyline, poster_image, trailer_url):
         self.title = movie_title
